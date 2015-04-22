73c9a7444353e895ffc275b18964e90ffd4844c3
Tomoya Tsuji <1020tomoya@gmail.com>
2015-04-22 17:41:49 +0900

    update

---
+++ ClassCategory/UIView+TTUtill.h
@@ -0,0 +1,14 @@
+//
+//  UIView+TTUtill.h
+//  TTUtill
+//
+//  Created by TsujiTomoya on 2015/04/22.
+//  Copyright (c) 2015年 TTUtill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (TTUtill)
+- (void)maskByRoundingCorners:(UIRectCorner)corners
+                 cornerRadius:(CGFloat)cornerRadius;
+@end
